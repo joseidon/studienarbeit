cf12c8c1a1fa91980b5e051fbd79c76b62837462
joseidon <61836330+joseidon@users.noreply.github.com>
2021-02-13 10:59:48 +0100

    knoten Objekt erstellt

---
+++ src/knoten.py
@@ -0,0 +1,14 @@
+class knoten:
+    type
+    id
+    def __init__(self):
+        print("test")
+    
+    def addType(self, type):
+        self.type = type
+    
+    def addID(self, id):
+        self.id = id
+
+    def
+
---
+++ src/nodereader.py
@@ -0,0 +1,5 @@
+
+
+def main():
+    while(true):
+        exit
